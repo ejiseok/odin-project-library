e1af689f5de7c93b305fba792e81a94d4d99b094
ejiseok <jiseoklee0502@gmail.com>
2026-03-30 20:27:24 +0900

    책 HTML 요소 생성 코드 작성

--- script.js
+++ script.js
@@ -28,6 +28,45 @@ const newBookBtn = document.querySelector(".new-book-btn");
 const createBookModal = document.querySelector(".create-book-modal");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
 
-newBookBtn.addEventListener("click", openCreateBookModal);
+const bookContainer = document.querySelector(".book-container");
 
+newBookBtn.addEventListener("click", openCreateBookModal);
 modalCloseBtn.addEventListener("click", closeCreateBookModal);
+
+myLibrary.forEach(bookInfo => {
+  const bookElement = document.createElement("div");
+  bookElement.classList.add("book");
+  
+  const bookInfoContainer = document.createElement("div");
+  bookInfoContainer.classList.add("book-info-container");
+  
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("title");
+  bookTitle.textContent = `${bookInfo.title}`;
+  
+  const bookPages = document.createElement("div");
+  bookPages.classList.add("pages");
+  bookPages.textContent = `${bookInfo.pages}p`;
+
+  bookInfoContainer.appendChild(bookTitle);
+  bookInfoContainer.appendChild(bookPages);
+
+  const bookBtnContainer = document.createElement("div");
+  bookBtnContainer.classList.add("book-btn-container");
+
+  const readBtn = document.createElement("button");
+  readBtn.classList.add(bookInfo.readed ? "readed-btn" : "read-btn");
+  readBtn.textContent = bookInfo.readed ? "Readed" : "Read";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.textContent = "Delete"
+
+  bookBtnContainer.appendChild(readBtn);
+  bookBtnContainer.appendChild(deleteBtn);
+
+  bookElement.appendChild(bookInfoContainer);
+  bookElement.appendChild(bookBtnContainer);
+
+  bookContainer.appendChild(bookElement);
+});
